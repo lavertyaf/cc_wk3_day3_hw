d94486460d879925da064ed83a576b4fc7a6990d
lavertyaf <lavertyaileen@gmail.com>
2018-03-21 17:43:37 +0000

    save function complete

---
+++ db/console.rb
@@ -0,0 +1,23 @@
+require('pg')
+require('pry')
+require_relative('../models/artist')
+require_relative('../models/album')
+
+# Artist.delete_all()
+# Album.delete_all()
+
+artist1 = Artist.new({ 'name' => 'Scott Hutchison' })
+artist2 = Artist.new({ 'name' => 'Sam McTrusty' })
+artist3 = Artist.new({ 'name' => 'Simon Neil' })
+
+artist1.save()
+artist2.save()
+artist3.save()
+
+album1 = Album.new({ 'name' => 'Pedestrian Verse', 'genre' => 'Folk Rock', 'artist_id' => artist1.id })
+album2 = Album.new({ 'name' => 'Vivarium', 'genre' => 'Guitar Rock', 'artist_id' => artist2.id })
+album3 = Album.new({ 'name' => 'Ellipsis', 'genre' => 'Rock', 'artist_id' => artist3.id })
+
+album1.save()
+album2.save()
+album3.save()
---
+++ models/album.rb
@@ -0,0 +1,28 @@
+require('pg')
+require('pry')
+require_relative('artist')
+require_relative('../db/sql_runner')
+
+class Album
+  attr_reader :id
+  attr_accessor :name, :genre, :artist_id
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @genre = options['genre']
+    @artist_id = options['artist_id']
+  end
+
+  def save()
+    sql = "INSERT INTO albums ( name,
+    genre,
+    artist_id)
+    VALUES
+    ( $1, $2, $3 )
+    RETURNING id"
+    values = [@name, @genre, @artist_id]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i
+  end
+
+end
